482c36a5ee1332077a348343d40ea8536488c743
Zexal0807 <gallinar00@gmail.com>
2020-09-07 10:38:25 +0200

    First implementation auth

--- util/callback.js
+++ util/callback.js
@@ -2,42 +2,94 @@ const validator = require("runtheons-validate");
 const ret = require("./ret");
 
 module.exports = (req, res, api) => {
-  data = [];
-  //metto in data i dati ricevuti nella query string
-  Object.keys(req.params).forEach(function(key){
-	let value = req.params[key];	
-	if(""+Number(value) == value){
-		data[key] = Number(value);
-	}else{
-		data[key] = value;
-	}
-  });
-  
-  //metto in data i dati ricevuti nel request body
-  Object.keys(req.body).forEach(function(key){
-	let value = req.body[key];	
-	if(""+Number(value) == value){
-		data[key] = Number(value);
-	}else{
-		data[key] = value;
-	}
-  });
-  
-  //metto in data i dati ricevuti come files
-  if(req.files){
-    Object.keys(req.files).forEach(function(key){
-      data[key] = req.files[key];
-    });
-  }
-  
-  try{
-  var valid = validator.validate(api.schema, data);
-    if(valid.result)
-      api.api(data, res);
-    else
-		res.json(ret({error:valid.errors}, false));
-  }catch(e){
-	  console.log(e);
-    res.jsonp(e.path + ': ' + e.message);
-  }
+
+    var authLevel = (api.auth != undefined ? api.auth : 0);
+    console.log(authLevel);
+    var authorized = authen(req, authLevel);
+    console.log(authorized);
+    if (authorized) {
+        data = [];
+        //metto in data i dati ricevuti nella query string
+        Object.keys(req.params).forEach(function(key) {
+            let value = req.params[key];
+            if ("" + Number(value) == value) {
+                data[key] = Number(value);
+            } else {
+                data[key] = value;
+            }
+        });
+
+        //metto in data i dati ricevuti nel request body
+        Object.keys(req.body).forEach(function(key) {
+            let value = req.body[key];
+            if ("" + Number(value) == value) {
+                data[key] = Number(value);
+            } else {
+                data[key] = value;
+            }
+        });
+
+        //metto in data i dati ricevuti come files
+        if (req.files) {
+            Object.keys(req.files).forEach(function(key) {
+                data[key] = req.files[key];
+            });
+        }
+        try {
+            var valid = validator.validate(api.schema, data);
+            if (valid.result)
+                api.api(data, res, req);
+            else
+                res.json(ret({ error: valid.errors }, false));
+        } catch (e) {
+            console.log(e);
+            res.jsonp(e.path + ': ' + e.message);
+        }
+    } else {
+        res.json(ret({ error: "User non authorized" }, false));
+    }
 };
+
+function authen(req, auth) {
+    /*
+    	.... for future
+    	IS_SOCIETY		0
+    	IS_COACH		0
+    	IS_PRO			0
+    	IS_ATHLETE		0
+    	IS_FAN			0
+    	LOGGED			0
+    */
+    //..000000 = 0	-> Not logged
+    //..000101 = 5	-> Logged as athlete
+
+    var bit = (auth).toString(2);
+    var length = 6;
+    for (let i = bit.length; i < length; i++) {
+        bit = "0" + bit;
+    }
+    bit = bit.split("");
+    bit = bit.reverse(); //Inverto per leggere da dx a sx
+    console.log(bit);
+
+    //bit 0 -> Logged/Not Logged
+    if (bit[0] == 1) {
+        //Non loggato quando dovrebbe
+        if (!req.session.idUser) {
+            return false;
+        }
+    } else {
+        //Loggato quando non serve (login)
+        if (req.session.idUser) {
+            return false;
+        }
+    }
+    //bit 1 -> Fan/Not Fan
+    if (bit[1] == 1) {
+        if (req.session.userType != 1) {
+            return false;
+        }
+    }
+
+    return true;
+}
