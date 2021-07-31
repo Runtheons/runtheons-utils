077df0de57ece787aba1f0fc95a03246f3b35d45
Zexal0807 <gallinar00@gmail.com>
2021-07-31 10:56:54 +0200

    Template return string on failure

--- utils/TemplateManager/index.js
+++ utils/TemplateManager/index.js
@@ -3,7 +3,10 @@ const ejs = require("ejs");
 exports.load = (filename, data, options = {}) => {
 	return new Promise((resolve, reject) => {
 		ejs.renderFile(filename, data, options, function(err, html) {
-			resolve(html);
+			if (err) {
+				return resolve("")
+			}
+			return resolve(html);
 		});
 	});
 };
