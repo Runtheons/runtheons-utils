ed44f6673567b54ebdcf517a52c0cf6badde8c63
Zexal0807 <gallinar00@gmail.com>
2021-08-06 11:14:32 +0200

    Implement model as static

--- utils/Model/Model.js
+++ utils/Model/Model.js
@@ -1,14 +1,20 @@
 class Model {
-	static config = {
-		separetor: "_",
+	static _config = {
+		separetor: ".",
+		includesSeparetor: ".",
 	};
-	static includes = {
-		gino: class A extends Model {},
-	};
+	static config = {};
+
+	static map(data) {
+		Object.assign(this.config, this._config);
+		Object.assign(this.includes, {});
+		var data = this._map(data);
+		this._include(data);
+		return new this(data);
+	}
 
 	constructor(data) {
-		var parsedData = Model._map(data);
-		Object.assign(this, parsedData);
+		Object.assign(this, data);
 	}
 
 	static _map(data) {
@@ -20,7 +26,7 @@ class Model {
 			var tmpObject = object;
 			var found;
 			do {
-				found = key.indexOf(Model.config.separetor);
+				found = key.indexOf(this.config.separetor);
 				if (found >= 0) {
 					var pre = key.substring(0, found);
 					key = key.substring(found + 1);
@@ -35,9 +41,33 @@ class Model {
 		});
 		return object;
 	}
+
 	static _include(data) {
-		Object.keys(this.includes).forEach((key) => {});
+		Object.keys(this.includes).forEach((key) => {
+			key = "" + key;
+			var tmpObject = data;
+			var pre = data;
+			var subkeys = key.split(this.config.includesSeparetor);
+			for (var i = 0; i < subkeys.length - 1; i++) {
+				var subkey = subkeys[i];
+				tmpObject = tmpObject[subkey];
+				pre = tmpObject;
+			}
+			var subdata = tmpObject[subkeys[subkeys.length - 1]];
+			var cl = this.includes[key].prototype.constructor;
+			var value = new cl(subdata);
+			pre[subkeys[subkeys.length - 1]] = value;
+		});
 	}
 }
-
+/*
+class B extends Model {}
+class A extends Model {
+	static includes = {
+		pippo: B,
+	};
+}
+var a = A.map({ gino: 1, "pippo.a": 1, "pippo.b": 2 });
+console.log(a);
+*/
 module.exports = Model;
