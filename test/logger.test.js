a47edad2c82c372426bd7c0e076ab75a4e876655
Zexal0807 <gallinar00@gmail.com>
2022-01-11 22:01:50 +0100

    Fix test

--- test/logger.test.js
+++ test/logger.test.js
@@ -3,17 +3,14 @@ const fs = require("fs");
 
 const { Logger } = require("./../index");
 
-describe("Token Generator", function() {
+describe("Logger", function() {
 	it("Example", async() => {
-		fs.rmdirSync("./debug", { recursive: true });
+		fs.emptyDirSync("./debug");
 
 		Logger.printDebugFile("TEST");
 
-		assert.equal(fs.existsSync("./debug"), true);
 		var files = fs.readdirSync("./debug");
 
 		assert.equal(files.length, 1);
-
-		fs.rmdirSync("./debug", { recursive: true });
 	});
 });
