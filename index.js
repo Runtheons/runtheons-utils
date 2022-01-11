8cec55108122b2aea8ca0929c7d11ffd6a474d4e
Zexal0807 <gallinar00@gmail.com>
2022-01-11 21:41:55 +0100

    Delete export config

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var config = () => {
 	const path = require("path");
 	var root = module.parent.filename;
@@ -5,11 +6,12 @@ var config = () => {
 
 	return require("./utils/Config/index.js")(root);
 };
+*/
 
 module.exports = {
 	CouponCode: require("./utils/CouponCode/index.js"),
 	Logger: require("./utils/Logger/index.js"),
 	TokenGenerator: require("./utils/TokenGenerator/index.js"),
 	TemplateManager: require("./utils/TemplateManager/index.js"),
-	Config: config(),
+	//Config: config(),
 };
