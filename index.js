3f6909756fbf4e124a661964395ab5f1c1971c86
Zexal0807 <gallinar00@gmail.com>
2022-01-25 17:21:29 +0100

    Remove config

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-/*
-var config = () => {
-	const path = require("path");
-	var root = module.parent.filename;
-	root = path.dirname(root);
-
-	return require("./utils/Config/index.js")(root);
-};
-*/
-
 module.exports = {
 	CouponCode: require("./utils/CouponCode/index.js"),
 	Logger: require("./utils/Logger/index.js"),
 	TokenGenerator: require("./utils/TokenGenerator/index.js"),
 	TemplateManager: require("./utils/TemplateManager/index.js"),
-	//Config: config(),
 };
