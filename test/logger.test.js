0ac67d9525371888a157d76318ff8eb0ed4105af
Zexal0807 <gallinar00@gmail.com>
2022-01-25 17:24:10 +0100

    Check dir exist

--- test/logger.test.js
+++ test/logger.test.js
@@ -1,18 +1,21 @@
 const assert = require("assert");
-const { execSync } = require("child_process");
 const fs = require("fs");
 
 const { Logger } = require("./../index");
 
 describe("Logger", function() {
 	it("Example", async() => {
-		fs.rmdirSync("./debug", { recursive: true });
+		if (fs.existsSync("./debug")) {
+			fs.rmdirSync("./debug", { recursive: true });
+		}
 
 		await Logger.printDebugFile("TEST");
 
 		var files = fs.readdirSync("./debug");
 
 		assert.equal(files.length, 1);
-		fs.rmdirSync("./debug", { recursive: true });
+		if (fs.existsSync("./debug")) {
+			fs.rmdirSync("./debug", { recursive: true });
+		}
 	});
 });
